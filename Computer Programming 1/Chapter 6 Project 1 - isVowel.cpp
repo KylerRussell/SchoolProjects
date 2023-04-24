c40f99c43b57d784425eb74d42935a33a3693df8
KylerRussell <928klr22@students.olatheschools.com>
2023-04-23 23:44:21 -0500

    Create Chapter 6 Project 1 - isVowel.cpp
    Start Chapter 6 skeleton

---
+++ Computer Programming 1/Chapter 6 Project 1 - isVowel.cpp
@@ -0,0 +1,17 @@
+/*
+Name: Kyler Russell
+Date: 4/23/23
+Desc: A program which determines if the letter in a word is a vowel.
+*/
+
+    #include <iostream>
+     
+    using namespace std;
+     
+    int main(){
+      	
+    	cout << "Hello World";
+      
+      	return 0;
+    }
+
